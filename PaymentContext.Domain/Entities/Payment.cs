7f18c83fca969ca7164bd1d7d26cb4fdae981e35
Ryan Gabriel <ryan.bromati@hotmail.com>
2023-04-05 17:16:28 -0300

    feat: add payment entity

---
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,26 @@
+namespace PaymentContext.Domain.Entities;
+
+public abstract class Payment
+{
+   public decimal Number { get; set; }       
+   public DateTime PaidDate  { get; set; }
+   public DateTime ExpireDate { get; set; }
+   public decimal Total { get; set; }
+   public decimal TotalPaid { get; set; }
+}
+
+public class BoletoPayment : Payment
+{
+   
+}
+
+
+public class CreditCardPayment : Payment
+{
+   
+}
+
+public class PaypalPayment : Payment
+{
+   
+}
---
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,9 @@
+namespace PaymentContext.Domain.Entities;
+
+public class Subscription
+{
+   public bool Active { get; set; }
+   public DateTime CreateDate { get; set; }
+   public DateTime LastUpdateDate { get; set; }
+   public DateTime? ExpiredDate { get; set; }
+}
